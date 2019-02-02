8d786a4d65f8334f3881ccff090378ed8ff994a9
fimmind <grayfox19@mail.ru>
2019-02-02 12:52:52 +0300

    Changes in 13-in-total * Split task to spans * change disign a few

--- 13-in-total.js
+++ 13-in-total.js
@@ -1,9 +1,9 @@
 var rightAnswer;
-var strSum;
+var nums = [1, 0, 0];
 
 function getRandomNumbers(){
-    let res = [0, 0, 0];
-    let currentSum = 0;
+    var res = [0, 0, 0];
+    var currentSum = 0;
     for(let i = 0; i < 3; ++i){
         let randNum = Math.floor(Math.random() * (13 - currentSum));
         res[i] = randNum;
@@ -13,12 +13,14 @@ function getRandomNumbers(){
 }
 
 function getTask(){
-    let nums = getRandomNumbers();
-    strSum = nums[0] + " + " + nums[1] + " + " + nums[2];
+    nums = getRandomNumbers();
     rightAnswer = 13 - nums[0] - nums[1] - nums[2];
 
-    $(".sum13title").html("");
-    $(".sum13task").html(strSum + " + __ = 13");
+    $(".title").html("");
+    for(let i = 0; i < 3; i++){
+        $(".task .num" + (i + 1)).html(nums[i]);
+    }
+    $(".task .inputedNumber").html("__")
 
     console.log(rightAnswer);
 }
@@ -27,17 +29,16 @@ $(document).ready(function() {
     getTask();
 
     for(let i = 1; i <= 12; ++i){
-        $(".number" + String(i)).click(function(){
+        $(".input .number" + String(i)).click(function(){
             if(i == rightAnswer){
-                $(".sum13title").html("Верно!")
-                $(".sum13task").html(strSum + " + " + rightAnswer + " = 13");
-                (new Promise((resolve) => setTimeout(resolve, 750))).then(()=>{
+                $(".title").html("Верно!");
+                $(".task .inputedNumber").html(i);
+                (new Promise((resolve) => setTimeout(resolve, 750))).then(function(){
                     getTask();
                 });
-
             }
             else{
-                $(".sum13title").html("Не верно...")
+                $(".title").html("Не верно...")
             }
         });
     }
