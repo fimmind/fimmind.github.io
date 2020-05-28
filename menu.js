fff45f2453f6553de82d916ecc1ff16d7e040ac7
fimmind <fimmind@mail.ru>
2020-05-28 23:36:40 +0300

    Realign everything by prettier

--- menu.js
+++ menu.js
@@ -1,44 +1,48 @@
 const currentHtmlFileName = window.location.pathname.split("/").pop();
 const menuIems = {
-    "Главная": "index.html",
-    "13 в сумме": "13-in-total.html",
-    "Остаток масти": "restofsuit.html",
-    "Cоздатель": "creator.html",
-}
+  Главная: "index.html",
+  "13 в сумме": "13-in-total.html",
+  "Остаток масти": "restofsuit.html",
+  Cоздатель: "creator.html",
+};
 
 var strMenuItems = "";
-for(var i in menuIems){
-    if(menuIems[i] === currentHtmlFileName){
-        strMenuItems += "       <li><p>" + i + "</p></li>\n";
-        continue;
-    }   // else
-    strMenuItems += "       <li><a href=\"" + menuIems[i] + "\">" + i + "</a></li>\n";
+for (var i in menuIems) {
+  if (menuIems[i] === currentHtmlFileName) {
+    strMenuItems += "       <li><p>" + i + "</p></li>\n";
+    continue;
+  } // else
+  strMenuItems +=
+    '       <li><a href="' + menuIems[i] + '">' + i + "</a></li>\n";
 }
 
 $("head")
-    .append('<link rel="stylesheet" href="menu.css">')
-    .append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+  .append('<link rel="stylesheet" href="menu.css">')
+  .append(
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+  );
 
-$(document).ready(function() {
-    $("body").prepend('\
+$(document).ready(function () {
+  $("body").prepend(
+    '\
     <div class="menuIcon">\
         <span class="menuIconLine"></span>\
         <span class="menuIconLine"></span>\
         <span class="menuIconLine"></span>\
     </div>\
     <nav>\
-        <lu>' + 
-            strMenuItems +
-        '</lu>\
-    </nav>');
+        <lu>' +
+      strMenuItems +
+      "</lu>\
+    </nav>"
+  );
 
-    $(".menuIcon, nav").hover(function() {
-        $("nav")
-            .stop()
-            .slideToggle(500);
-    }, function() {
-        $("nav")
-            .stop()
-            .slideToggle(500);
-    });
+  $(".menuIcon, nav").hover(
+    function () {
+      $("nav").stop().slideToggle(500);
+    },
+    function () {
+      $("nav").stop().slideToggle(500);
+    }
+  );
 });
